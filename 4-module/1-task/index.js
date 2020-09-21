1d4b2c909ba2873d02f505bc094c6fc0f52d9b22
Sergey Milevich <sergey.milevich@monterosa.co.uk>
2020-09-21 13:26:14 +0300

    04module-01task-milevich

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,4 +5,12 @@
  */
 function makeFriendsList(friends) {
   // ваш код...
+  const newULElement = document.createElement("ul");
+
+  friends.map((item) => {
+    const newliElenent = document.createElement("li");
+    newliElenent.innerHTML = item.firstName + " " + item.lastName;
+    newULElement.append(newliElenent)
+  })
+  return newULElement
 }
